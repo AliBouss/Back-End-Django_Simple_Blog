c6224e753b58621b0d2dca79ec83247956e954c8
Alibouss <bouss58@hotmail.com>
2021-04-06 13:25:50 +0200

    ajout des articles et config de l'administration

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
+# notre fonction qui va permettre de créer notre clés étrangère
+from django.template.defaultfilters import slugify
+
+User = get_user_model()
+
 # Create your models here.
+class BlogPost(models.Model):
+
+    title = models.CharField(max_length=255, unique=True, verbose_name="Titre")
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    last_update = models.DateTimeField(auto_now=True)
+    created_on = models.DateField(blank=True, null=True)
+    published = models.BooleanField(default=False, verbose_name="Publié")
+    content = models.TextField(blank=True, verbose_name="Contenu")
+
+    class Meta:
+        ordering = ["-created_on"]
+        verbose_name = "Article"
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+
+        super().save(*args, **kwargs)
+
+
